2be8a0f4810ef12b6f62566cee24abab04d29b99
Neo <neojay0705@gmail.com>
2024-12-13 19:12:04 +0800

    first commit

---
+++ src/main/java/net/n/example/high_concurrent_java_server/HighConcurrentJavaServerApplication.java
@@ -0,0 +1,13 @@
+package net.n.example.high_concurrent_java_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighConcurrentJavaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighConcurrentJavaServerApplication.class, args);
+	}
+
+}
